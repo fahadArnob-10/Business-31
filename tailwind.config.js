1a0d44597b84be733abc988b4c72e93258cff10a
fahadArnob-10 <fahadarnob67677@gmail.com>
2024-09-03 01:39:39 +0600

    font

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,6 +2,11 @@
 module.exports = {
   content: ["*"],
   theme: {
+    fontFamily: {
+      nunito: "Nunito, sans-serif",
+      syne: "Syne, sans-serif",
+      poppins: "Poppins, sans-serif",
+    },
     extend: {},
   },
   plugins: [],
